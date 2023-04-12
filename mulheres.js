64f886ea9b69e9473fde8f9e28e17dfcccfcdd0c
angelegito <angelegito@hotmail.com>
2023-04-11 22:28:02 -0300

    atualizando o código mulheres

--- mulheres.js
+++ mulheres.js
@@ -1,22 +1,20 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router()
+
 
 const app = express();
 const porta = 3333;
 
-function mostraHora(request, response) {
-
-    const data = new Date()
-   
-    const hora = data.toLocaleTimeString('pt-BR')
-   
-    response.send(hora)
-   
+function mostraMulher(request,response) {
+    response.json({
+        nome: 'Angélica Egito',
+        imagem: 'https://avatars.githubusercontent.com/u/82975477?v=4',
+        minibio:'Desenvolvedora e Mãe'
+    })
 }
 
 function mostraPorta() {
     console.log("Servidor criado e rodando na porta ", porta);
 }
-
-app.use(router.get(''))
+app.use(router.get('/mulher', mostraMulher))
 app.listen(porta, mostraPorta);
